ab1dda60baaf0e58a2792d0b13e63b224b2ff452
y.dubovitsky <y.dubovitsky@gmail.con>
2019-02-22 16:02:03 +0300

    23. Апплеты

---
+++ manual/src/main/java/applet23/page838/AppletShow.java
@@ -0,0 +1,36 @@
+package applet23.page838;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ *
+ */
+public class AppletShow extends Applet {
+
+//    public void  init() {
+//
+//    }
+//
+//    public void start() {
+//
+//    }
+
+//    public void stop() {
+//
+//    }
+//
+//    public void destroy() {
+//
+//    }
+//
+//    public void paint(Graphics g) {
+//
+//    }
+
+    public static void main(String[] args) {
+        AppletShow appletShow = new AppletShow();
+        appletShow.init();
+        appletShow.start();
+    }
+}

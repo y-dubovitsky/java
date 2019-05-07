85e9ecbfd8924951ba76c96c09feecca6e683185
y.dubovitsky <y.dubovitsky@gmail.con>
2019-05-07 10:23:56 +0300

    package GUI.window:
    - adding button with changing text after clicking.
    
    TODO:
    
    - improve application;

--- other/src/main/java/GUI/window/ButtonEventListener.java
+++ other/src/main/java/GUI/window/ButtonEventListener.java
@@ -1,5 +1,6 @@
 package GUI.window;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,6 +22,10 @@ public class ButtonEventListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        //TODO Это не универсально!
+        if (this.component instanceof JButton) {
+            ((JButton) this.component).setText("Hello");
+        }
         this.component.repaint();
     }
 }
--- other/src/main/java/GUI/window/StartApp.java
+++ other/src/main/java/GUI/window/StartApp.java
@@ -12,11 +12,21 @@ public class StartApp {
      */
     public static void main(String[] args) {
         Frame frame = new Frame(500, 500);
+
+        // one button
         Button button = new Button("Кнопка");
         button.addListener(new ButtonEventListener(frame.getjFrame()));
 
-        // one and more Listener
+        // two button
+        Button button1 = new Button("Кнопка");
+        button1.addListener(new ButtonEventListener(button1.getjButton()));
+
+        // one Listener
         frame.addWidgets(BorderLayout.CENTER, new RectWidget());
         frame.addWidgets(BorderLayout.EAST, button.getjButton());
+
+        // two Listener
+        frame.addWidgets(BorderLayout.NORTH, new RectWidget());
+        frame.addWidgets(BorderLayout.SOUTH, button1.getjButton());
     }
 }

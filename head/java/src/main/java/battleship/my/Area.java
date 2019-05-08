0c815b7455edf6f93a9fad1d160c9f7050df5613
y.dubovitsky <y.dubovitsky@gmail.con>
2019-05-08 11:42:35 +0300

    Java Project:
    - reworked the structure of the project;
    - changed pom.xml;
    - added parent pom.xml;
    - etc...

---
+++ head/java/src/main/java/battleship/my/Area.java
@@ -0,0 +1,15 @@
+package battleship.my;
+
+import dot.DotCom;
+
+/**
+ * This class describe battle area;
+ */
+public class Area {
+    // Size of battle area
+    int[] size;
+
+    public Area(int size) {
+        this.size = new int[size];
+    }
+}

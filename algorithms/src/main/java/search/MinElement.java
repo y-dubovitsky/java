cbeb710d89d653475258a2787b12eeca12b0dc75
y.dubovitsky <y.dubovitsky@gmail.con>
2020-06-29 14:14:37 +0300

    Searching the minimum element in an array.

---
+++ algorithms/src/main/java/search/MinElement.java
@@ -0,0 +1,27 @@
+package search;
+
+/**
+ * Поиск минимального элемента в массиве.
+ */
+class MinElement { //TODO Можно улучшить, сделать дженерик и т.п.
+
+    private static final int[] INPUT_ARRAY = {1,5,-567, 34, 545, 654, 023, 234};
+
+    public static void main(String[] args) {
+        new MinElement().minElementSearch(INPUT_ARRAY);
+    }
+
+    private int minElementSearch(int[] array) {
+        int min = array[0];
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                index = i;
+            }
+        }
+        System.out.println("Min element = " + min + " index : " + index);
+        return min;
+    }
+
+}
